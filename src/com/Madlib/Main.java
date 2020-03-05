1ce78056f07c84a6f5fdddc8a150df6a33c7d335
barsha <barsha.sangraula.2022@students.lhschools.org>
2020-03-05 11:22:56 -0500

    OddnessMethodCreated

--- src/com/Madlib/Main.java
+++ src/com/Madlib/Main.java
@@ -1,8 +1,32 @@
 package com.Madlib;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	testOddness();
+
     }
+    public static void testOddness() {
+        //todo even or odd
+        //todo keep number contained
+        //todo get the last digit
+
+
+        //We want to find if a number is even or odd
+        int number;
+
+
+        Scanner keyboard;
+        keyboard= new Scanner((System.in));
+
+        System.out.println("Give me a positive number ");
+        number=keyboard.nextInt();
+        System.out.println("Number us even: " + (number % 2 == 0));
+        System.out.println("Number is odd: " + (number % 2 != 0));
+
+    }
+
 }
+
